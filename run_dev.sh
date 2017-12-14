fdedb912ef4236d543148a6cb53a078359d8f82f
Youngbok Yoon <master@bluehack.net>
2017-12-14 21:18:21 +0900

    Fixed container name mismatched problem

--- run_dev.sh
+++ run_dev.sh
@@ -6,4 +6,4 @@ GRPC_PORT=50051
 sudo nvidia-docker run -dit --restart unless-stopped \
     -e RELEASE_MODE=$RELEASE_MODE \
     -e GRPC_PORT=$GRPC_PORT \
-    -p $GRPC_PORT:$GRPC_PORT bluelens/bl-detect:$RELEASE_MODE
+    -p $GRPC_PORT:$GRPC_PORT bluelens/bl-feature:$RELEASE_MODE
